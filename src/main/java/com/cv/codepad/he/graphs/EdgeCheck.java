e1e315b1cce28d924934e1b2b1d4c4ecdf90f740
Chandravadan S <s.chandravadan@gmail.com>
2016-12-27 11:05:57 +0530

    Graphs basic tutorial

---
+++ src/main/java/com/cv/codepad/he/graphs/EdgeCheck.java
@@ -0,0 +1,45 @@
+package com.cv.codepad.he.graphs;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Chandravadan on 12/27/2016.
+ */
+public class EdgeCheck {
+    public static void main(String args[]) throws Exception {
+        //BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        //Input number of nodes and edges
+        String line = br.readLine();
+        String[] parts = line.split(" ");
+        int nodes = Integer.parseInt(parts[0].trim());
+        int edges = Integer.parseInt(parts[1].trim());
+        boolean[][] graph = new boolean[nodes][nodes];
+
+        //Input edges
+        for (int i = 0; i < edges; i++) {
+            line = br.readLine();
+            parts = line.split(" ");
+            int node1 = Integer.parseInt(parts[0].trim());
+            int node2 = Integer.parseInt(parts[1].trim());
+            graph[node1][node2] = true;
+            graph[node2][node1] = true;
+        }
+
+        //Take queries
+        line = br.readLine();
+        int queries = Integer.parseInt(line.trim());
+        for (int i = 0; i < queries; i++) {
+            line = br.readLine();
+            parts = line.split(" ");
+            int node1 = Integer.parseInt(parts[0].trim());
+            int node2 = Integer.parseInt(parts[1].trim());
+            if (graph[node1][node2])
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+    }
+}
