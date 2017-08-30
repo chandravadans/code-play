406a3bf322a875f161ea7ceeba44afd4eefab29b
Chandravadan S <s.chandravadan@gmail.com>
2017-08-30 09:28:39 +0530

    Spoj problems - ADDREV improvement, Fast IO 2

--- src/main/java/com/cv/codepad/spoj/ADDREV/Main.java
+++ src/main/java/com/cv/codepad/spoj/ADDREV/Main.java
@@ -2,6 +2,7 @@ package com.cv.codepad.spoj.ADDREV;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 /**
  * Created by Chandravadan on 8/30/2017.
@@ -11,10 +12,9 @@ public class Main {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(in.readLine());
         while(t > 0) {
-            String line = in.readLine();
-            String[] parts = line.split(" ");
-            int a = Integer.parseInt(new StringBuffer(parts[0]).reverse().toString());
-            int b = Integer.parseInt(new StringBuffer(parts[1]).reverse().toString());
+            StringTokenizer tokenizer = new StringTokenizer(in.readLine());
+            int a = Integer.parseInt(new StringBuffer(tokenizer.nextToken()).reverse().toString());
+            int b = Integer.parseInt(new StringBuffer(tokenizer.nextToken()).reverse().toString());
             int ans = a + b;
             System.out.println(Integer.parseInt(new StringBuffer(String.valueOf(ans)).reverse().toString()));
             t--;
---
+++ src/main/java/com/cv/codepad/spoj/PRIME1/Main.java
@@ -0,0 +1,10 @@
+package com.cv.codepad.spoj.PRIME1;
+
+/**
+ * Created by Chandravadan on 8/30/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
